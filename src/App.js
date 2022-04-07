a437a299d2b8e731d850c375b8185eed2c01be90
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-04-07 09:04:42 +0700

    properti/props pada komponen

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App () {
   return (
     <div className="App">
       <header className="App-header">
-        <Hello />
+        <Hello name='Mastah' gretting='Selamat Datang'/>
       </header>
     </div>
   )
--- src/Hello.js
+++ src/Hello.js
@@ -1,5 +1,29 @@
-function Hello () {
-  return <h1>Hello!</h1>
+/*
+function Hello (props) {
+  return `Hello ${props.name}!`
+}
+*/
+
+/*
+function Hello (props) {
+  const { name } = props
+  return `Hello ${name}!`
+}
+*/
+
+/*
+function Hello ({ name }) {
+  return `Hello ${name}!`
+}
+*/
+
+function Hello ({ name, gretting }) {
+  return `Hello ${name}, ${gretting}!`
+}
+
+// nilai default pada props
+Hello.defaultProps = {
+  name: 'anonim'
 }
 
 export default Hello
