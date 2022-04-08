d57c55572a626b1d2c00cce6421c6df1cbb6bce8
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-04-08 21:19:14 +0700

    (menangani form) uncontrolled form - update

--- src/UncontrolledForm.js
+++ src/UncontrolledForm.js
@@ -2,22 +2,26 @@ import * as React from 'react'
 
 const UncontrolledForm = () => {
   const inputName = React.useRef(null)
+  const inputPhoto = React.useRef(null)
 
   const handleSubmit = event => {
     event.preventDefault()
     // kirim data form
     alert(inputName.current.value)
+    // File API untuk dapat nama file
+    alert(inputPhoto.current.files[0].name)
   }
 
   return (
-  <form onSubmit={handleSubmit}>
-    <label>
-        Name: <input type='text' ref={inputName} defaultValue='alex' />
-        {/* Catatan: Untuk <input type='checkbox'> and <input type='radio'> kita bisa pakai defaultChecked,
-        sedangkan <select> and <textarea> kita bisa menggunakan defaultValue. */}
-    </label>
-    <input type='submit' value='Submit' />
-  </form>
+    <form onSubmit={handleSubmit}>
+      <label>
+          Name: <input type='text' ref={inputName} defaultValue='Alex' />
+      </label>
+      <label>
+          Photo: <input type='file' ref={inputPhoto} />
+      </label>
+      <input type='submit' value='Submit' />
+    </form>
   )
 }
 
