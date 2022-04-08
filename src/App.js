15ed4dc8311bba8f83b1a016e95c7131c516efc3
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-04-08 08:48:20 +0700

    (menangani event) event pada elemen html

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import './App.css'
-import List from './List'
-import Table from './Table'
-import users from './User'
+import AllEvent from './AllEvent'
 
 function App () {
   return (
     <div >
-     {/* <List users={users}/> */}
-     <Table users={users}/>
+     <AllEvent />
     </div>
   )
 }
