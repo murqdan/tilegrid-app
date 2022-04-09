a95fa89f34a4fe9f8743f41d987f5980823f6a00
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-04-09 14:47:27 +0700

    (menangani form) validasi dasar pada multiple field form

--- src/FormBasicValidation.js
+++ src/FormBasicValidation.js
@@ -2,35 +2,57 @@ import * as React from 'react'
 import InputText from './InputText'
 
 const FormBasicValidation = () => {
-  const [name, setName] = React.useState('')
-  const [error, setError] = React.useState('field minimal 3 karakter')
+  const [form, setForm] = React.useState({
+    name: '',
+    phone: '',
+    email: ''
+  })
+  const [error, setError] = React.useState({
+    name: 'name minimal 3 karakter',
+    phone: 'phone minimal 3 karakter',
+    email: 'email minimal 3 karakter'
+  })
 
   const handleSubmit = event => {
     event.preventDefault()
-    if (error) {
-      alert(error)
+
+    if (error.name.length > 0) {
+      alert(error.name)
+    } else if (error.phone.length > 0) {
+      alert(error.phone)
+    } else if (error.email.length > 0) {
+      alert(error.email)
     } else {
-      // sent to server
-      alert(name)
+      alert('Validasi sukses')
     }
   }
 
-  const handleChange = (e) => {
+  const handleChange = (field, e) => {
     // validasi disini
     setError('')
     const value = e.target.value
     if (value.length < 3) {
-      setError('field minimal 3 karakter')
+      setError({ ...error, [field]: `${field} minimal 3 karakter` })
+    } else {
+      setError({ ...error, [field]: '' })
     }
-    setName(value)
+    setForm({ ...form, [field]: value })
   }
 
   return (
     <form onSubmit={handleSubmit}>
-        {error}
+        {error.name}
         <br/>
-        <InputText label='Name' value={name} onChange={handleChange} />
+        <InputText label='Name' value={form.name} onChange={(e) => handleChange('name', e)} />
+        <br/><br/>
+        {error.phone}
         <br/>
+        <InputText label='Phone' value={form.phone} onChange={(e) => handleChange('phone', e)} />
+        <br/><br/>
+        {error.email}
+        <br/>
+        <InputText label='Email' value={form.email} onChange={(e) => handleChange('email', e)} />
+        <br/><br/>
         <input type='submit' value='Submit' />
     </form>
   )
