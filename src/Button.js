e44a89e60f182e19531ae0ee57d7b4eee04b0fd8
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-04-10 08:00:42 +0700

    (Pengecekan Tipe Data Pada Props) - update

--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,5 @@
 import style from './Button.module.scss'
+import PropTypes from 'prop-types'
 
 const Button = ({ props }) => {
   return (
@@ -6,6 +7,10 @@ const Button = ({ props }) => {
       {props}
     </button>
   )
+}
+
+Button.propTypes = {
+  props: PropTypes.string
 }
 
 export default Button
