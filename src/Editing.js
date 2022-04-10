013bf7efa84e1f857801d395ea1f1c76b8fc1cf4
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-04-10 17:37:18 +0700

    (useEffect) effect dependencies

--- src/Editing.js
+++ src/Editing.js
@@ -2,21 +2,30 @@ import { useState, useEffect } from 'react'
 
 const Editing = () => {
   const [text, setText] = useState('') // <--- state
+  const [seconds, setSeconds] = useState(0) // <--- kita buat state baru = seconds
 
   useEffect(function () {
+    console.log('sinkronisasi ke local storage')
     if (text.length) localStorage.setItem('edit-text', text)
-  })
+  }, [text])
 
   useEffect(function () {
     setText(localStorage.getItem('edit-text'))
+    setInterval(() => {
+      setSeconds(second => second + 1) // increment seconds
+    }, 1000) // <--- jalankan setiap 1000 milidetik (1 detik)
   }, [])
 
   return (
-    <textarea
-        type="text"
-        value={text} // <--- value mengambil nilai text
-        onChange={e => setText(e.target.value)} // <--- update state text ketika nilai textarea berubah;
-    />
+    <>
+        Waktu: {Math.floor(seconds / 60)} menit {seconds % 60} detik
+        <br/>
+        <textarea
+            type="text"
+            value={text}
+            onChange={e => setText(e.target.value)}
+        />
+    </>
   )
 }
 
