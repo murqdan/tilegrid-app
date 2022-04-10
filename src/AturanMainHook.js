4fc95daa317e9389f2ec5b6a64490ebd6b53164c
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-04-10 10:21:35 +0700

    (aturan main hook)

---
+++ src/AturanMainHook.js
@@ -0,0 +1,2 @@
+// 1. Hook harus dipanggil dari fungsi komponen atau custom hook
+// 2. Hook tidak boleh dipanggil dari dalam looping, conditional --if else-- ataupun dari fungsi di dalam fungsi (nested function).
