21af50a832aa9eae74783ef81b31393dc991a546
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-04-10 21:03:40 +0700

    (useEffect) clean up function

--- src/Editing.js
+++ src/Editing.js
@@ -11,20 +11,29 @@ const Editing = () => {
 
   useEffect(function () {
     setText(localStorage.getItem('edit-text'))
-    setInterval(() => {
+
+    // kita tangkap id interval ke dalam variabel `intervalId`
+    const intervalId = setInterval(() => {
       setSeconds(second => second + 1) // increment seconds
     }, 1000) // <--- jalankan setiap 1000 milidetik (1 detik)
+
+    // ------ CLEAN UP FUNCTION ---------//
+    // fungsi ini hanya akan dipanggil setelah komponen unmounted
+    return function () {
+      clearInterval(intervalId) // <--- panggil clearInterval di sini
+    }
+    // ---------------------------------//
   }, [])
 
   return (
     <>
-        Waktu: {Math.floor(seconds / 60)} menit {seconds % 60} detik
-        <br/>
-        <textarea
-            type="text"
-            value={text}
-            onChange={e => setText(e.target.value)}
-        />
+      Waktu: {Math.floor(seconds / 60)} menit {seconds % 60} detik
+      <br/>
+      <textarea
+        type="text"
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
     </>
   )
 }
