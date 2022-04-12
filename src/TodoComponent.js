0b2c8b4d791d4fcc508b63f1deb1b016ff6ad1eb
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-04-12 14:02:45 +0700

    (useReducer) penggunaan usereducer untuk membuat todolist - update

--- src/TodoComponent.js
+++ src/TodoComponent.js
@@ -2,7 +2,6 @@ import { useReducer } from 'react'
 
 function reducer (todos, action) {
   switch (action.type) {
-    // -------------------------
     case 'ADD_TODO': {
       const nextId = todos.length ? todos[todos.length - 1].id + 1 : 1
       return [...todos, {
@@ -11,7 +10,15 @@ function reducer (todos, action) {
         ...action.todo
       }]
     }
-    // -------------------------
+
+    case 'REMOVE_TODO':
+      return todos.filter(todo => todo.id !== action.id)
+
+    case 'UPDATE_TODO':
+      return todos.map(todo => todo.id === action.todo.id ? action.todo : todo)
+
+    case 'TOGGLE_COMPLETED':
+      return todos.map(todo => todo.id === action.id ? { ...todo, completed: !todo.completed } : todo)
 
     default:
       return todos
@@ -29,7 +36,27 @@ const TodoComponent = props => {
             title: 'Belajar hooks'
           }
         })}> ADD TODO </button>
-        {todos}
+
+        <button onClick={e => dispatch({
+          type: 'REMOVE_TODO',
+          id: 1 // <--- sesuai ID todo yang ingin dihapus
+        })}> REMOVE TODO </button>
+
+        <button onClick={e => dispatch({
+          type: 'UPDATE_TODO',
+          todo: {
+            id: 1, // <--- sesuai todo yang ingin diupdate
+            title: 'Judul todo baru'
+            // ... dst data lainnya
+          }
+        })}> UPDATE TODO </button>
+
+        <button onClick={e => dispatch({
+          type: 'TOGGLE_COMPLETED',
+          id: 1 // <---- sesuai ID todo yang ingin di _toggle_
+        })}> TOGGLE </button>
+
+        {console.log(todos[0])}
     </div>
 }
 
