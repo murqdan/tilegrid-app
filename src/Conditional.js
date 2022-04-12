5ad057d2228487c15bf15add495d1de666fa9356
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-04-12 17:43:19 +0700

    (useLayoutEffect)

--- src/Conditional.js
+++ src/Conditional.js
@@ -1,5 +1,3 @@
-import * as React from 'react'
-
 const Conditional = () => {
   const isLogin = true
   const userName = 'Alex'
---
+++ src/UseLayoutEffect.js
@@ -0,0 +1,3 @@
+// useLayoutEffect biasanya digunakan untuk menghitung nilai yang hanya bisa didapatkan saat DOM selesai diupdate.
+// Misalnya untuk menangkap nilai lebar halaman atau viewport.
+// Cara penggunaan dan bentuk dari fungsinya pun sama persis seperti useEffect
