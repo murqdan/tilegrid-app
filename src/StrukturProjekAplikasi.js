7234e774decd2e5e06ea6e2b838203a08c86f5f0
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-04-13 10:02:00 +0700

    (struktur projek aplikasi)

---
+++ src/StrukturProjekAplikasi.js
@@ -0,0 +1,5 @@
+/* Secara umum, setidaknya ada dua pendekatan dalam menyusun struktur projek
+sebagaimana disebutkan oleh ReactJS pada website resminya, yaitu:
+
+1. Mengelompokkan berdasarkan fitur atau routing
+2. Mengelompokkan berdasarkan jenis file */
