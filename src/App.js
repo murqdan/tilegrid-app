b4ea4317b5997af1656f4921c5e91418f2c8e916
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-04-13 14:34:52 +0700

    (routing aplikasi) mengenal kompone utama pada react router - route matcher

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import BelajarCustomHook from './BelajarCustomHook'
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+
+const Home = () => <h2>Halaman Home</h2>
+const About = () => <h2>Halaman About</h2>
 
 function App () {
   return (
-    <div>
-      <BelajarCustomHook />
+    <div className="App App-header">
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
     </div>
   )
 }
