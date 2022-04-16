8e40eebd33f0e9d52b189e27ceb4d80b392d2664
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-04-16 08:46:02 +0700

    (state management) membuat aplikasi grid - menghubungkan redux dengan komponen

--- src/App.js
+++ src/App.js
@@ -1,58 +1,13 @@
-import React from 'react'
-import { Routes, Route, NavLink } from 'react-router-dom'
-import './App.css'
-const GuardRoute = React.lazy(() => import('./components/GuardRoute'))
-const Category = React.lazy(() => import('./pages/Category'))
-const Drinks = React.lazy(() => import('./pages/Drinks'))
-const Member = React.lazy(() => import('./pages/Member'))
-const Login = React.lazy(() => import('./pages/Login'))
-const Foods = React.lazy(() => import('./pages/Foods'))
-const About = React.lazy(() => import('./pages/About'))
-const Post = React.lazy(() => import('./pages/Post'))
-const Home = React.lazy(() => import('./pages/Home'))
-const Notfound = React.lazy(() => import('./pages/Notfound'))
+import store from './app/store'
+import { Provider } from 'react-redux'
 
 function App () {
-  const [isLogin, setLogin] = React.useState(false)
-
   return (
-    <React.Suspense fallback={<div>Loading...</div>}>
-      <div className="App">
-        <ul className='menu'>
-          <li><NavLink to="/" className={(navData) => (navData.isActive ? 'active' : 'none')}>Home</NavLink></li>
-          <li><NavLink to="/about" className={(navData) => (navData.isActive ? 'active' : 'none')}>About</NavLink></li>
-          <li><NavLink to="/member" className={(navData) => (navData.isActive ? 'active' : 'none')}>Member</NavLink></li>
-          <li><NavLink to="/category" className={(navData) => (navData.isActive ? 'active' : 'none')}>Category</NavLink></li>
-          <li>{isLogin
-            ? <NavLink to="/logout" onClick={(e) => {
-              e.preventDefault()
-              setLogin(false)
-            }} >Logout</NavLink>
-            : <NavLink to="/login" className={(navData) => (navData.isActive ? 'active' : 'none')}>Login</NavLink>
-          }</li>
-        </ul>
-        <div className="main">
-          <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route
-            path="/member"
-            element={
-              <GuardRoute isLogin={isLogin}>
-                <Member/>
-              </GuardRoute>
-            }/>
-            <Route path="/login" element={<Login setLogin={setLogin}/>}/>
-            <Route path="/category" element={<Category/>}>
-              <Route path="foods" element={<Foods />} />
-              <Route path="drinks" element={<Drinks />} />
-            </Route>
-            <Route path="/post/:id" element={<Post/>}/>
-            <Route path="*" element={<Notfound/>}/>
-          </Routes>
-        </div>
-      </div>
-    </React.Suspense>
+    <div>
+      <Provider store={store}>
+
+      </Provider>
+    </div>
   )
 }
 
