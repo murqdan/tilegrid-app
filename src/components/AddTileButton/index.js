c2a3c764b7eef005d69b7dd91f90957cbeb5b864
Muhammad Rafiqul Adnan <murqdan@gmail.com>
2022-04-16 13:33:48 +0700

    (state management) membuat aplikasi grid - menghubungkan redux dengan komponen - membuat komponen AddTileButton

---
+++ src/components/AddTileButton/index.js
@@ -0,0 +1,20 @@
+import { string, func } from 'prop-types'
+
+export default function AddTileButton ({ onClick, color }) {
+  const buttonStyles = {
+    backgroundColor: color,
+    display: 'inline-block',
+    width: 20,
+    height: 20,
+    cursor: 'pointer',
+    border: '1px solid black'
+  }
+  return (
+    <div style={buttonStyles}/>
+  )
+}
+
+AddTileButton.propTypes = {
+  onClick: func.isRequired,
+  color: string.isRequired
+}
